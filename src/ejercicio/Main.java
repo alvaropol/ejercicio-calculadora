091751b666087800243df9be73e37fe35503736f
Álvaro Polonio Domínguez <alvaro.pold@gmail.com>
2023-04-17 09:24:48 +0200

    menu

--- src/ejercicio/Main.java
+++ src/ejercicio/Main.java
@@ -1,34 +1,12 @@
 package ejercicio;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 
-		double num1, num2,result;
-		int menu=0;
-		Scanner sc = new Scanner(System.in);
-		
-		do {
-			
-		
-		System.out.println("1-Sumar\n"
-				+ "2-Restar\n"
-				+ "3-Multiplicar\n"
-				+ "4-Dividir\n"
-				+ "5-Resto de una operación\n"
-				+ "0-SALIR");
-		menu=Integer.parseInt(sc.nextLine());
-		
-		switch(menu) {
-		
-		
-		
-		}
-		
-		} while (menu!=0);
 	}
 
 }
